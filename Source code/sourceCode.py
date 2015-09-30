0950a6f6d1cbedd8694e981059e847b0a7e17f17
aruizga7 <armand.ruiz@gmail.com>
2015-09-29 23:03:05 -0500

    Adding Files

--- Source code/sourceCode.py
+++ Source code/sourceCode.py
@@ -1,92 +1,48 @@
-from pyspark.context import SparkContext
-from pyspark.sql.context import SQLContext
 
-from pyspark.mllib.recommendation import ALS, MatrixFactorizationModel, Rating
 import spss.pyspark.runtime
-import json
-import uuid
 
-user_field = '%%user_field%%'
-item_field = '%%item_field%%'
-rating_field = '%%rating_field%%'
-model_path =  '%%model_path%%'
-rank = %%rank%%
-iterations = %%iterations%%
-lmbda = %%lmbda%%
-blocks = %%blocks%%
-random_seed = long(%%random_seed%%)
+from pyspark.sql.types import StringType, IntegerType, DoubleType, StructField, StructType
+
+source_field = '%%source_field%%'
+dest_field = '%%dest_field%%'
+iterations = %%iteration_count%%
+damping = %%damping_factor%%
 
 ascontext = spss.pyspark.runtime.getContext()
-sc = ascontext.getSparkContext()
-df = ascontext.getSparkInputData()
 
-dummypath = ascontext.createTemporaryFolder()
+sqlCtx = ascontext.getSparkSQLContext()
 
-def saveDummy(sc, path)
-    data = sc.parallelize([json.dumps({dummyTrue})])
-    data.saveAsTextFile(path)
+out_schema = StructType([
+    StructField("ID", StringType(), nullable=True),
+    StructField("Rank", DoubleType(), nullable=True)
+    ])
 
-def getIndexes(frame)
-    schema = frame.dtypes[]
-    indexes = {}
-    for i in range(0,len(schema))
-        (cname,ctype) = schema[i]
-        indexes[cname] = i
+ascontext.setSparkOutputSchema(out_schema)
 
-    u_index = indexes[user_field]
-    i_index = indexes[item_field]
-    r_index = indexes[rating_field]
+if not ascontext.isComputeDataModelOnly():
+    df = ascontext.getSparkInputData()
+    from_to = df.rdd.map(lambda x: (getattr(x,source_field),getattr(x,dest_field))).distinct() # (from,to)
 
-    return (u_index,i_index,r_index)
+    def computeContribs(urls, rank):
+        num_urls = len(urls)
+        for url in urls:
+            yield (url, rank / num_urls)
 
-(user_index,item_index,rating_index) = getIndexes(df)
+    links = from_to.mapValues(lambda x:[x]).reduceByKey(lambda x,y: x+y) # links: (from,[to,to,to...])
 
+    N = links.count()
 
-def metaMap(row,u_index,i_index,r_index)
-    user = row[u_index]
-    item = row[i_index]
-    rating = row[r_index]
-    s1 = set()
-    s2 = set()
-    s1.add(user)
-    s2.add(item)
-    return (s1,s2,{rating1})
+    ranks = links.map(lambda (url, neighbors): (url, 1.0/float(N))) # ranks: (from,rank)
 
-def metaReduce(meta1,meta2)
-    users = meta1[0].union(meta2[0])
-    items = meta1[1].union(meta2[1])
-    ratings = {}
-    for k in meta1[2].keys()
-        ratings[k] = meta1[2][k]
-    for k in meta2[2].keys()
-        if k not in ratings
-            ratings[k] = 0
-        ratings[k] += meta2[2][k]
-    return (users,items,ratings)
+    for iteration in xrange(0,iterations):
+        contribs = links.join(ranks).flatMap(
+                lambda (url, (urls, rank)): computeContribs(urls, rank))  # contribs: (to, contribution_from)
 
-# perform datapass to compute stats
+        ranks = contribs.reduceByKey(lambda x,y:x+y).mapValues(lambda rank: rank * damping + (1-damping)/N)
 
-rating_stats = df.rdd.map(lambda row metaMap(row,user_index,item_index,rating_index)).reduce(lambda x,ymetaReduce(x,y))
-print(Total number of distinct items + str(len(rating_stats[1])))
-print(Total number of distinct users + str(len(rating_stats[0])))
+    outdf = sqlCtx.createDataFrame(ranks,out_schema)
 
-print(Rating frequencies)
-ratings_range = sorted(rating_stats[2].keys())
-for rating in ratings_range
-    print(t+str(rating)+ +str(rating_stats[2][rating]))
+    ascontext.setSparkOutputData(outdf)
 
-ratings = df.map(lambda l Rating(int(l[user_index]), int(l[item_index]), float(l[rating_index])))
-
-# Build the recommendation model using Alternating Least Squares
-
-model = ALS.train(ratings, rank, iterations, lmbda, blocks, seed=random_seed)
-
-# Save and load model
-model.save(sc, model_path)
-saveDummy(sc,dummypath)
-ascontext.setModelContentFromPath(dummy,dummypath)
-
-
-
-
+    
 
